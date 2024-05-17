2fb4527671f95fc1f9124cbf73fd4371b20d4c67
Andrey Potapov <aapot@ya.ru>
2024-05-17 03:01:56 +0300

    Validators for User and Post

--- users/serializers.py
+++ users/serializers.py
@@ -7,7 +7,12 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'first_name', 'last_name', 'email', 'phone_number', 'date_of_birth']
-        write_only_fields = ['password']
+
+    def validate_email(self, value):
+        if value:
+            if 'mail.ru' not in value.lower() and 'yandex.ru' not in value.lower():
+                raise serializers.ValidationError("Можно добавлять почту только mail.ru или yandex.ru")
+        return value
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
@@ -16,6 +21,11 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'password']
         write_only_fields = ['password']
+
+    def validate_password(self, value):
+        if len(value) > 8 and any(char.isdigit() for char in value):
+            return value
+        raise serializers.ValidationError("Пароль должен быть не менее 8 символов и включать цифры")
 
     def to_representation(self, instance):
         return {'username': instance.username}
