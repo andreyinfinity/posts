1cc542c366750a725e819a4c6f69c49ee22244c9
Andrey Potapov <aapot@ya.ru>
2024-05-18 21:36:10 +0300

    Admin site configured

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from posts.models import Post, Comment
 
-# Register your models here.
+
+class CommentInline(admin.TabularInline):
+    """Инлайн для отображения комментариев в объекте публикации"""
+    model = Comment
+    readonly_fields = (
+        'date_posted',
+        'author',
+        'content'
+    )
+    show_change_link = True
+    can_delete = False
+    extra = 0
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Отображение списка публикаций"""
+    list_display = (
+        'id',
+        'title',
+        'author',
+        'content',
+        'date_posted',
+        'date_modified'
+    )
+    list_display_links = ('title',)
+    list_filter = ('date_posted',)
+    inlines = [CommentInline]
+    readonly_fields = (
+        'date_posted',
+        'date_modified',
+        'author'
+    )
--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,56 @@
 from django.contrib import admin
 
+from posts.models import Post, Comment
 from users.models import User
+
+
+class PostInline(admin.TabularInline):
+    """Инлайн для отображения публикаций в объекте пользователя"""
+    model = Post
+    readonly_fields = (
+        'date_posted',
+        'title',
+        'content',
+        'picture'
+    )
+    show_change_link = True
+    can_delete = False
+    extra = 0
+
+
+class CommentInline(admin.TabularInline):
+    """Инлайн для отображения комментариев в объекте пользователя"""
+    model = Comment
+    readonly_fields = (
+        'date_posted',
+        'post',
+        'content',
+    )
+    show_change_link = True
+    can_delete = False
+    extra = 0
+    show_full_result_count = True
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     """Отображение списка пользователей"""
-    list_display = ['pk', 'username']
+    list_display = (
+        'pk',
+        'username',
+        'first_name',
+        'last_name',
+        'email',
+        'phone_number',
+    )
+    list_display_links = ('username',)
+    list_filter = ('groups',)
+    inlines = [PostInline, CommentInline]
+    readonly_fields = (
+        'password',
+        'date_joined',
+        'last_login',
+        'date_of_creation',
+        'date_of_edition'
+    )
+
